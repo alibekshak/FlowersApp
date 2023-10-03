c1fa6a1566830f532be7eb3f572042cfb39b76eb
alibekshak <alibekshakiro1@gmail.com>
2023-10-03 14:54:30 +0600

    first

---
+++ Flower/FlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerApp.swift
+//  Flower
+//
+//  Created by Apple on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
