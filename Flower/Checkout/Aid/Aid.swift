90f980992dca02dfee0100aef86fc050c222bb9d
alibekshak <alibekshakiro1@gmail.com>
2023-10-05 12:01:43 +0600

    Created CheckoutView and dir Aid

---
+++ Flower/Checkout/Aid/Aid.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct Aid{
+    
+    struct CheckoutNameOfBlock{
+        var navigationName: String = "Оформление заказа"
+        var address: String = "Адрес"
+        var addInfo: String = "Дополнительная информация"
+        var addPostcard: String = "Добавить открытку"
+        var timeDelivery: String = "Время доставки"
+        var orderPrice: String = "Сумма заказа"
+    }
+    
+    struct PickerName{
+        var delivery: String = "Доставка"
+        var pickup: String = "Самовывоз"
+    }
+    
+}
